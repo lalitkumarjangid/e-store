57c85083f65f85cd9f5079e33826b54e19173d11
Lalit Kumar Jangid <lkjlalitkumar2005@gmail.com>
2023-11-02 14:59:01 +0530

    Deployed

---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import Category from "./Category/Category";
+import Price from "./Price/Price";
+import Colors from "./Colors/Colors";
+import "./Sidebar.css";
+
+const Sidebar = ({ handleChange }) => {
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <section className={`sidebar ${isFixed ? "fixed" : ""}`}>
+        <div className="logo-container">
+          <h1>🛒</h1>
+        </div>
+        <Category handleChange={handleChange} />
+        <Price handleChange={handleChange} />
+        <Colors handleChange={handleChange} />
+      </section>
+    </>
+  );
+};
+
+export default Sidebar;
